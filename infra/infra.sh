a5bf5eb0e05c3767be7a372b461999aa0cfc7216
iamtienng <iamtienng@gmail.com>
2024-09-24 22:53:10 +0200

    first commit

---
+++ infra/infra.sh
@@ -0,0 +1,19 @@
+# Install minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
+# Create the folder that will be copied into the control plane.
+mkdir -p ~/.minikube/files/etc/ca-certificates/
+
+# Set the sops age private key
+export SOPS_AGE_KEY='' # replace with your age private key
+
+# Decrypt the token.csv file
+sops --decrypt -i token.csv
+
+# Copy the token file into the folder.
+cp token.csv ~/.minikube/files/etc/ca-certificates/token.csv
+
+# Start the minikube / Same command in Windows
+minikube start --driver docker --container-runtime docker --gpus all --extra-config=apiserver.token-auth-file=/etc/ca-certificates/token.csv --addons metrics-server
+
