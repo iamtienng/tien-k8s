7d7994f5a687458a719c9c559abeb6ac510bf07e
Tien Nguyen <iamtienng@gmail.com>
2024-09-24 23:08:12 +0200

    feat: infra.sh

--- infra/infra.sh
+++ infra/infra.sh
@@ -17,3 +17,6 @@ cp token.csv ~/.minikube/files/etc/ca-certificates/token.csv
 # Start the minikube / Same command in Windows
 minikube start --driver docker --container-runtime docker --gpus all --extra-config=apiserver.token-auth-file=/etc/ca-certificates/token.csv --addons metrics-server
 
+# Set GitHub personal access token and username:
+export GITHUB_TOKEN=''
+export GITHUB_USER=''
